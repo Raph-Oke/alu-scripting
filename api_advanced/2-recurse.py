3c0c1f6ff2979496a736e815acaafb7c69f6e888
root <r.musoke@alustudent.com>
2024-03-04 15:02:48 +0000

    one

--- api_advanced/2-recurse.py
+++ api_advanced/2-recurse.py
@@ -1,16 +1,32 @@
 #!/usr/bin/python3
-"""
-2-main
-"""
-import sys
+""" 2-recurse.py """
+import requests
 
-if __name__ == '__main__':
-    recurse = __import__('2-recurse').recurse
-    if len(sys.argv) < 2:
-        print("Please pass an argument for the subreddit to search.")
-    else:
-        result = recurse(sys.argv[1])
-        if result is not None:
-            print(len(result))
+
+def recurse(subreddit, hot_list=[], after=None):
+    """ returns list with titles of all hot articles in a subreddit """
+    url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
+    headers = {'User-Agent': 'Mozilla/5.0'}
+    params = {'after': after}
+    response = requests.get(
+                                url,
+                                headers=headers,
+                                params=params,
+                                allow_redirects=False
+                            )
+    if response.status_code == 200:
+        data = response.json().get('data')
+        if data is not None:
+            children = data.get('children')
+            if children is not None:
+                for child in children:
+                    hot_list.append(child.get('data').get('title'))
+                after = data.get('after')
+                if after is not None:
+                    return recurse(subreddit, hot_list, after)
+                else:
+                    return hot_list
         else:
-            print("None")
+            return hot_list
+    else:
+        return None
